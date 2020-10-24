f6880c62aeac4fc57a9d8f43df6167eb30939dd9
sugsugsug <thddnrud2010@naver.com>
2020-10-24 18:39:42 +0900

    multi image and css added

--- frontend/src/containers/MainPage/Popup/FoodCategoryPopup/FoodCategoryPopup.js
+++ frontend/src/containers/MainPage/Popup/FoodCategoryPopup/FoodCategoryPopup.js
@@ -5,6 +5,17 @@ import * as actionTypes from '../../../../store/actions/actionTypes';
 import { withRouter } from 'react-router';
 import * as actionCreators from '../../../../store/actions/index';
 import KoreanImage from './img/Korean.jpeg'
+import VietnamImage from './img/Vietnam.jpeg'
+import ChineseImage from './img/Chinese.jpeg'
+import ChickenImage from './img/Chicken.jpeg'
+
+const foodCategoryName = [
+  "Korean",
+  "Chinese",
+  "Western",
+  "Vietnam",
+]
+
 
 // props = {
 //   user_id
@@ -16,6 +27,8 @@ class FoodCategoryPopup extends Component {
       Korean: false,
       Western: false,
       Chinese: false,
+      Vietnam: false,
+      Chicken: false,
     },
   }
 
@@ -37,19 +50,37 @@ class FoodCategoryPopup extends Component {
     this.props.onEditUserFoodCategory(id_and_foodCategory)
     this.props.closepopup()
   }
- // we need to add hover and clicked img showing
+  // we need to add hover and clicked img showing
+  // DB needed for image
   render() {
     return (
     <div className='FoodCategoryPopup'>
-      <button>
-        <img 
-          src={KoreanImage} 
+        <img className={this.state.foodCategory.Korean ? 'ClickedImage' : 'unClickedImage'}
+          src={KoreanImage}
           alt="Korean" 
-          onClick={
-            () => this.postClickFoodCategoryHandler("Korean")
-          }>
+          onClick={ () => this.postClickFoodCategoryHandler("Korean") }
+        >
         </img>
-      </button>     
+        <img className={this.state.foodCategory.Chinese? 'ClickedImage' : 'unClickedImage'}
+          src={ChineseImage}
+          alt="Chinese" 
+          onClick={ () => this.postClickFoodCategoryHandler("Chinese") }
+        >
+        </img>
+        <br/>
+        <img className={this.state.foodCategory.Vietnam ? 'ClickedImage' : 'unClickedImage'}
+          src={VietnamImage}
+          alt="Vietnam" 
+          onClick={ () => this.postClickFoodCategoryHandler("Vietnam") }
+        >
+        </img>
+        <img className={this.state.foodCategory.Chicken ? 'ClickedImage' : 'unClickedImage'}
+          src={ChickenImage}
+          alt="Chicken" 
+          onClick={ () => this.postClickFoodCategoryHandler("Chicken") }
+        >
+        </img>
+      <br/>
       <button onClick={() => this.postClickSaveHandler()}>
         Save
       </button>
