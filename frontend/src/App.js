9cec7910e66cd0ad584d12029580e634186ba116
sugsugsug <thddnrud2010@naver.com>
2020-10-29 00:59:02 +0900

    fixed: App.js recovered

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React from 'react';
+
+import MainPage from './containers/MainPage/MainPage'
+import './App.css';
+
+function App() {
+  return (
+    // temporarily using path '/' for MainPage for testing
+    <BrowserRouter>
+      <div className = "App">
+        <Switch>
+          <Route path = '/' exact render = {() => <MainPage/>} />
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
