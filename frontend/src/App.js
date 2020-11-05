b796ad8f4d4da2dfb72f27d20daee222bf0263b5
mushypeas <qkrwoans2@gmail.com>
2020-11-05 10:49:56 +0900

    initial SignIn build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import React from 'react';
 
-import MainPage from './containers/MainPage/MainPage'
-import PreferencePopup from './containers/MainPage/Popup/PreferencePopup/PreferencePopup.js';
-import './App.css';
 import DetailPage from './containers/DetailPage/DetailPage';
+import SignIn from "./containers/SignIn/SignIn";
+import MainPage from './containers/MainPage/MainPage'
+import './App.css';
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
     <BrowserRouter>
       <div className = "App">
         <Switch>
-          <Route path = '/' exact render = {() => <MainPage/>} />
-          <Route path='/main'exact component={MainPage}></Route>
+          <Route path='/sign-in' exact render={() => <SignIn/>}/>
+          <Route path='/main' exact component={MainPage}/>
           <Route path='/main/:name' exact render={()=><MainPage/>}/>
           <Route paht='/main/detail/:id' exact render={()=><DetailPage/>}/>
           <Route render={() => <h1>Not Found</h1>} />
---
+++ frontend/src/containers/SignIn/SignIn.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+import * as actionCreators from '../../store/actions/index';
+
+class SignIn extends Component{
+  state = {
+    email: "",
+    password: "",
+  }
+  onClickSignInHandler = () => {
+    this.props.onPostSignIn(this.state.email, this.state.password);
+  }
+  render(){
+    return (
+      <div className="sign-in">
+        <img id="logo"></img>
+        <div>나만의 입맛, 나만의 평점.</div>
+        <div id="sign-in-box">
+          <h1>로그인</h1>
+          <div>이메일</div>
+          <input type="text" id="email-input" value={this.state.email}
+            onChange={(event) => this.setState({ email: event.target.value })} />
+          <div>비밀번호</div>
+          <input type="text" id="password-input" value={this.state.password}
+            onChange={(event) => this.setState({ password: event.target.value })} />
+          <button id="sign-in-button" onClick = {()=>this.onClickSignInHandler()} >로그인</button>
+          <a id="sign-up-link" href={this.props.history.push("/sign-up/")}>회원 가입</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStatetoProps = state => {
+  return {
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onPostSignIn: (email, password) => {
+      dispatch(actionCreators.postSignIn(email, password));
+    },
+  }
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(withRouter(SignIn));
