08dc70fd1fabd3db99cfb6fba1af65ab009b5204
sugsugsug <thddnrud2010@gmail.com>
2020-11-07 10:29:16 +0900

    RestaurantSummary.js modified

--- frontend/src/components/MainPage/RestaurantSummary/RestaurantSummary.js
+++ frontend/src/components/MainPage/RestaurantSummary/RestaurantSummary.js
@@ -1,26 +1,26 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 
-class RestaurantSummary extends Component{
-    render(){
-        //이미지랑 타이틀 위에다가 링크 걸기할거에요~ 링크를 아직 몰라서
-        //<NavLink to >
-        return(
-            <div className='restaurantSummary'>
-                <div className='left'>
-                    
-                    <img src={this.props.img_url} width='100' height='100' 
-                    onClick={()=>this.props.clickrestaurant()}/>
-                </div>
-                <div className='right'>
-                    <p onClick={()=> this.props.clickrestaurant() }>
-                        {this.props.title}</p>
-                    <p>{this.props.rate} </p>
-                </div>
-     
-            </div>
-        );
-    };
+class RestaurantSummary extends Component {
+  onClickRestaurantHandler = () => {
+    this.props.history.push(`/detail/${this.props.restaurantID}/`);
+  }
+
+  render() {
+    return(
+      <div className='restaurantSummary'>
+        <div className='left'>
+          <img src={this.props.imgURL} width='100' height='100' 
+            onClick={() => this.onClickRestaurantHandler()}/>
+        </div>
+        <div className='right'>
+          <p onClick={() => this.onClickRestaurantHandler()}>{this.props.name}</p>
+          <p>{this.props.rating} </p>
+          <p>{this.props.foodCategory} </p>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default RestaurantSummary;
