180a586a5a71c835bf591a4a71611e34d7c89b2d
mushypeas <qkrwoans2@gmail.com>
2020-11-14 14:56:49 +0900

    - fixed eslint errors for: CreatID, restaurantActions

--- frontend/src/containers/SignUp/CreateID/CreateID.js
+++ frontend/src/containers/SignUp/CreateID/CreateID.js
@@ -13,7 +13,9 @@ const backgroundStyle = {
 };
 
 class CreateID extends Component {
-    state={
+  constructor(props) {
+    super(props);
+    this.state = {
       userInfo: {
         username: null,
         email: null,
@@ -22,89 +24,93 @@ class CreateID extends Component {
       },
       verifyPassword: null,
       mode: 'ID',
-    }
+    };
+  }
 
-    onClickConfirmHandler() {
-      this.setState({ mode: 'Preference' });
-    }
+  onClickConfirmHandler() {
+    this.setState({ mode: 'Preference' });
+  }
 
-    onChangeButtonHandler() {
-      if (this.state.userInfo.username === null || this.state.userInfo.password === null
-            || this.state.userInfo.email === null) return true;
-      if (this.state.userInfo.password === this.state.verifyPassword) return false;
-      return true;
-    }
+  onChangeButtonHandler() {
+    const { userInfo } = this.state;
+    if (userInfo.username === null || userInfo.password === null
+            || userInfo.email === null) return true;
+    if (userInfo.password === this.state.verifyPassword) return false;
+    return true;
+  }
 
-    render() {
-      if (this.state.mode === 'Preference') this.props.onChangeStageHandler(this.state.userInfo);
-      // return(
-      // <CreatePreferenceVector  userInfo={this.state.userInfo}/>
-      // );
-      let isverified;
-      if (this.state.userInfo.password === null
-            || this.state.userInfo.password != this.state.verifyPassword) isverified = 'Password not verified';
-      else isverified = 'Ok';
-      return (
+  render() {
+    const { userInfo } = this.state;
+    if (this.state.mode === 'Preference') this.props.onChangeStageHandler(userInfo);
+    // return(
+    // <CreatePreferenceVector  userInfo={userInfo}/>
+    // );
+    let isverified;
+    if (userInfo.password === null
+        || userInfo.password !== this.state.verifyPassword) {
+      isverified = 'Password not verified';
+    } else isverified = 'Ok';
+    return (
 
-      // <div className='createID' style={ backgroundStyle} >
-        <div className="createID">
-          <div className="box">
-            <text className="signup">회원가입</text>
-            <text className="step1">STEP1</text>
+    // <div className='createID' style={ backgroundStyle} >
+      <div className="createID">
+        <div className="box">
+          <text className="signup">회원가입</text>
+          <text className="step1">STEP1</text>
 
-            <p>이름</p>
-            <input
-              id="username-input"
-              type="text"
-              value={this.state.userInfo.username}
-              onChange={(ev) => {
-                this.setState({ userInfo: { ...this.state.userInfo, username: ev.target.value } });
-              }}
-            />
+          <p>이름</p>
+          <input
+            id="username-input"
+            type="text"
+            value={userInfo.username}
+            onChange={(ev) => {
+              this.setState({ userInfo: { ...userInfo, username: ev.target.value } });
+            }}
+          />
 
-            <p>이메일</p>
-            <input
-              id="email-input"
-              type="text"
-              value={this.state.userInfo.email}
-              onChange={(ev) => {
-                this.setState({ userInfo: { ...this.state.userInfo, email: ev.target.value } });
-              }}
-            />
+          <p>이메일</p>
+          <input
+            id="email-input"
+            type="text"
+            value={userInfo.email}
+            onChange={(ev) => {
+              this.setState({ userInfo: { ...userInfo, email: ev.target.value } });
+            }}
+          />
 
-            <p>비밀번호</p>
-            <input
-              id="password-input"
-              type="password"
-              value={this.state.userInfo.password}
-              onChange={(ev) => {
-                this.setState({ userInfo: { ...this.state.userInfo, password: ev.target.value } });
-              }}
-            />
-            <p>비밀번호확인</p>
-            <input
-              id="verify-password-input"
-              type="password"
-              value={this.state.verifyPassword}
-              onChange={(ev) => {
-                this.setState({ verifyPassword: ev.target.value });
-              }}
-            />
-            <p id="verified">{isverified}</p>
+          <p>비밀번호</p>
+          <input
+            id="password-input"
+            type="password"
+            value={userInfo.password}
+            onChange={(ev) => {
+              this.setState({ userInfo: { ...userInfo, password: ev.target.value } });
+            }}
+          />
+          <p>비밀번호확인</p>
+          <input
+            id="verify-password-input"
+            type="password"
+            value={this.state.verifyPassword}
+            onChange={(ev) => {
+              this.setState({ verifyPassword: ev.target.value });
+            }}
+          />
+          <p id="verified">{isverified}</p>
 
-            <button
-              id="confirm-button"
-              disabled={this.onChangeButtonHandler()}
-              onClick={() => this.onClickConfirmHandler()}
-            >
-              확인
-            </button>
-
-          </div>
+          <button
+            id="confirm-button"
+            disabled={this.onChangeButtonHandler()}
+            onClick={() => this.onClickConfirmHandler()}
+          >
+            확인
+          </button>
 
         </div>
-      );
-    }
+
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
