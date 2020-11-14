567ed3c64d5760ad45966ec8678f2566f9588f2f
mushypeas <qkrwoans2@gmail.com>
2020-11-14 11:00:28 +0900

    - finished SearchResult unittest

---
+++ frontend/src/components/SearchResult/SearchResult.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SearchResult from './SearchResult';
+
+
+describe('<SearchResult />', () => {
+  it('should get props properly', () => {
+    const component = shallow(<SearchResult address_name = "옹헤야"/>);
+    const wrapper = component.find('.searchResult');
+    expect(wrapper.length).toBe(1);
+  });
+});
