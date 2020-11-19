7404be26e439771e4dcd96dfd307c4112df2ac4e
mushypeas <qkrwoans2@gmail.com>
2020-11-19 14:42:06 +0900

    struggling with selenium&django

--- backend/crawling/kakao_crawling.py
+++ backend/crawling/kakao_crawling.py
@@ -1,53 +1,34 @@
-from bs4 import BeautifulSoup
-import requests
-from urllib.request import urlopen
-import json
-from collections import OrderedDict
-import time
+# import sys
+# sys.path.append('/home/mushypeas/workspace/project/backend/')
+# from ATM.models import Restaurant
+
+# from selenium.common.exceptions import ElementClickInterceptedException
+# from selenium.common.exceptions import ElementNotInteractableException
+# from selenium.common.exceptions import ElementNotVisibleException
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.support import expected_conditions as WHEN
+from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 from selenium import webdriver
-from konlpy.tag import Kkma
-from konlpy.tag import Okt
-from konlpy.tag import Hannanum
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementNotVisibleException
-from selenium.common.exceptions import ElementNotInteractableException
+from collections import OrderedDict
+from urllib.request import urlopen
+# from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('./chromedriver')
-driver.implicitly_wait(3)
+import requests
+import json
+import time
+
+
+browser = webdriver.Chrome('backend/crawling/chromedriver')
+browser.implicitly_wait(3)
 url = 'https://map.kakao.com/'
-driver.get(url)
-i = 0
-while True:
-  try:
-    i += 1
-    driver.find_element_by_class_name("RestaurantReviewList__MoreReviewButton").click()
-    driver.find_element_by_id("search.keyword.query").send_keys("")
-    if i > 50:
-      break
-    # driver.find_element_by_class_name("_3iTUo").click()
-    # time.sleep(2)
-  except NoSuchElementException:
-    break
-  except ElementNotInteractableException:
-    try:
-      driver.find_element_by_class_name("close_icon").click()
-    except:
-      break
-# reviews = driver.find_elements_by_class_name("WoYOw")
-reviews = driver.find_elements_by_class_name("RestaurantReviewItem__ReviewText")
-print(len(reviews))
-reviews_text = ""
-for review in reviews:
-  reviews_text += (review.text + " ")
-han = Hannanum()
-tokens = han.morphs(reviews_text)
-keywords = {}
-for token in tokens:
-  if token in keywords:
-    keywords[token] += 1
-  else:
-    keywords[token] = 1
-
-for keyword in keywords:
-  if keywords[keyword] >= 5 and len(keyword) >= 2:
-    print(keyword + ": " + str(keywords[keyword]))
+browser.get(url)
+browser.find_element_by_id("search.keyword.query").send_keys('샤로수길 음식점' + Keys.RETURN)
+links = browser.find_elements_by_class_name("moreview")
+restaurants = []
+ActionChains(browser).click(links[0]).perform() # don't know why but it doesn't work without this
+for link in links:
+  restaurant = {}
+  ActionChains(browser).click(link).perform()
