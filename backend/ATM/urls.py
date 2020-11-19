789dda79dd32db1b08a33bba3703863374965871
Choi JinWoo <sailing5a@snu.ac.kr>
2020-11-20 02:40:56 +0900

    review backend implemented and corresponding frontend actions are fixed

--- backend/ATM/urls.py
+++ backend/ATM/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('restaurant/<int:restaurant_id>/other-review/', views.get_other_reviews, name='other-review'),
+    path('myreview/', views.post_my_review, name='post-my-review'),
+    path('myreview/<int:review_id>/', views.edit_my_review, name='edit-my-review'),
+
 ]
