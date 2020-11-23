8c205d3098ae2a0fbab3f848a4adc337f391ba1d
jtlho <jtlho@jtlhoui-MacBookPro.local>
2020-11-23 11:55:02 +0900

    d

--- backend/ATM/restaurant/urls.py
+++ backend/ATM/restaurant/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-
-    path('restaurant/search/<str:word>', views.searched_restaurants, name = 'searched-restauratns'),
+    path('restaurant/search/<str:word>', views.searched_restaurants, name = 'searched_restauratns'),
     path('restaurant/detail/<int:id>', views.restaurant, name = 'restaurant'),
-
 ]
