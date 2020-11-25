d7192acebef5b9b2473176065f253e6fa85c43b1
jtlho <jtlho@jtlhoui-MacBookPro.local>
2020-11-26 00:15:58 +0900

    customized_rating temp

--- backend/ATM/restaurant/views.py
+++ backend/ATM/restaurant/views.py
@@ -5,17 +5,21 @@ from django.contrib.auth import authenticate,login,logout
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 from json import JSONDecodeError
-from .models import Article,Comment
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Restaurant, openTime, menu, ThumbNail, keyword, Review, PreferenceVector, Profile, Location
+from ..models import Restaurant, openTime, menu, ThumbNail, keyword, Review, PreferenceVector, Profile, Location
 from haversine import haversine
-
+from ..utils import cos_sim_word
 #preferencVector
 def searched_restaurants(request,word):
     if request.method == 'GET':
         if request.user.is_authenticated == False:
             return HttpResponse(status = 401)
         author = Profile.objects.get(user=request.user)
+        author_pref_vec = user.preference_vector
+        author_attr_list = get_preference_attributes(author_pref_vec)
+        author_pref_dict = {}
+        for attr in author_attr_list:
+            author_pref_dict[attr] = author_pref_vec[attr]
         if word == '':
             response_list = []
             for restaurant in Restaurant.objects.all().values():
@@ -27,16 +31,15 @@ def searched_restaurants(request,word):
                 response_dict['id'] = restaurant.id
                 response_dict['title'] = restaurant.name
                 response_dict['category'] = restaurant.foodCategory
-                response_dict['rate'] = 3.5
                 thumbnail = 
                     ThumbNail.objects.get(restaurant_name=restaurant.name).select_related('restaurant')
                 response_dict['img_url'] = thumbnail.url
-                pref_vec = restaurant.preferenceVector
-                attr_list = get_preference_attributes(pref_vec)
-                preferencevector_dict = {}
-                for attr in attr_list:
-                    preferencevector_dict[attr] = pref_vec[attr]
-                res = sorted(preferencevector_dict.items, key=f2, reverse=True)
+                restaurant_pref_vec = restaurant.preferenceVector
+                restaurant_attr_list = get_preference_attributes(restaurant_pref_vec)
+                restaurant_pref_dict = {}
+                for attr in restaurant_attr_list:
+                    restaurant_pref_dict[attr] = restaurant_pref_vec[attr]
+                res = sorted(restaurant_pref_dict.items, key=f2, reverse=True)
                 i = 0
                 sorted_dict = {}
                 for pref in res:
@@ -45,7 +48,7 @@ def searched_restaurants(request,word):
                     sorted_dict[pref] = res[pref]
                     i += 1
                 response_dict['preferenceVector'] = sorted_dict
-
+                response_dict['rate'] = get_customized_rating(restaurant_pref_dict, author_pref_dict )
                 response_list.append(response_dict)
             return JsonResponse(response_list, safe=False, status = 200)
         else:
@@ -59,16 +62,15 @@ def searched_restaurants(request,word):
                 response_dict['id'] = restaurant.id
                 response_dict['title'] = restaurant.name
                 response_dict['category'] = restaurant.foodCategory
-                response_dict['rate'] = 3.5
                 thumbnail=
                     ThumbNail.objects.get(restaurant_name=restaurant.name).select_related('restaurant')
                 response_dict['img_url'] = thumbnail.urls
-                pref_vec = restaurant.preferenceVector
-                attr_list = get_preference_attributes(pref_vec)
-                preferencevector_dict = {}
-                for attr in attr_list:
-                    preferencevector_dict[attr] = pref_vec[attr]
-                res = sorted(preferencevector_dict.items, key=f2, reverse=True)
+                restaurant_pref_vec = restaurant.preferenceVector
+                restaurant_attr_list = get_preference_attributes(restaurant_pref_vec)
+                restaurant_pref_dict = {}
+                for attr in restaurant_attr_list:
+                    restaurant_pref_dict[attr] = restaurant_pref_vec[attr]
+                res = sorted(restaurant_pref_dict.items, key=f2, reverse=True)
                 i = 0
                 sorted_dict = {}
                 for pref in res:
@@ -77,7 +79,8 @@ def searched_restaurants(request,word):
                     sorted_dict[pref] = res[pref]
                     i += 1
                 response_dict['preferenceVector'] = sorted_dict
-
+                response_dict['rate'] = get_customized_rating(restaurant_pref_dict, author_pref_dict )
+             
                 response_list.append(response_dict)
           
             return JsonResponse(response_list, safe= False, status = 200)
@@ -96,11 +99,23 @@ def restaurant(request,id) :
         except Restaurant.DoesNotExist:
             return HttpResponse(status = 404)
         else:
+            author = Profile.objects.get(user=request.user)
+            author_pref_vec = user.preference_vector
+            author_attr_list = get_preference_attributes(author_pref_vec)
+            author_pref_dict = {}
+            for attr in author_attr_list:
+                author_pref_dict[attr] = author_pref_vec[attr]
+            restaurant_pref_vec = restaurant.preferenceVector
+            restaurnat_attr_list = get_preference_attributes(restaurant_pref_vec)
+            restaurant_pref_dict = {}
+            for attr in restaurant_attr_list:
+                restaurant_pref_dict[attr] = restaurant_pref_vec[attr]
+                
             response_dict = {}
             response_dict['id'] = restaurant.id
             response_dict['name'] = restaurant.name
             response_dict['category'] = restaurant.foodCategory
-            response_dict['rate'] = 3.5
+            response_dict['rate'] = get_customized_rating(restaurant_pref_dict, author_pref_dict )
             response_dict['difference'] = 3.5 - restaurant.avgRating
             thumbnail_list = []
             for thumbnail in ThumbNail.objects.filter(restaurant_name=restaurant.name).select_related('restaurant'):
@@ -136,3 +151,11 @@ def get_preference_attributes(pref_vec):
         if attr not in no_need_attr:
             new_attr_list.append(attr)
     return new_attr_list
+
+def get_customized_rating(restaurant_pref, user_pref, avg_rating):
+    diff = 0
+    for restaurant_factor in restaurant_pref:
+        for user_factor in user_pref:
+            similarity = cos_sim_word(user_factor, restaurant_factor)
+            diff += similarity * (0.5 - abs(restaurant_pref[restaurant_factor]- user_pref[user_factor]))
+    return avg_rating + diff
