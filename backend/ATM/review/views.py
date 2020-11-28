5a950a89a6bbf44a2fd3bba940bd2ca6c528658b
Choi JinWoo <sailing5a@snu.ac.kr>
2020-11-28 16:53:49 +0900

    review conflict resolved

--- backend/ATM/review/views.py
+++ backend/ATM/review/views.py
@@ -20,11 +20,14 @@ def get_other_reviews(request, restaurant_id):
             reviews_on_target = Review.objects.filter(restaurant=target)
 
             naver = [{'id': review.id, 'content': review.content, 'rating': review.rating, 'date': review.date.strftime(
-                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username} for review in reviews_on_target if review.site == 'naver']
+                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username}
+                for review in reviews_on_target if review.site == 'naver']
             kakao = [{'id': review.id, 'content': review.content, 'rating': review.rating, 'date': review.date.strftime(
-                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username} for review in reviews_on_target if review.site == 'kakao']
+                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username}
+                for review in reviews_on_target if review.site == 'kakao']
             atm = [{'id': review.id, 'content': review.content, 'rating': review.rating, 'date': review.date.strftime(
-                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username} for review in reviews_on_target if review.site == 'atm']
+                '%Y/%m/%d, %H:%M:%S'), 'author_name': review.author.user.username}
+                for review in reviews_on_target if review.site == 'atm']
 
             other_review_list = {'naver': naver, 'kakao': kakao, 'atm': atm}
             other_review_list = {}
@@ -33,10 +36,6 @@ def get_other_reviews(request, restaurant_id):
                 safe=False)  # default status is 200
         else:
             return HttpResponseNotAllowed(['GET'])
-<<<<<<< HEAD
-=======
-
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
     else:
         return HttpResponse(status=401)
 
@@ -50,22 +49,9 @@ def get_post_my_review(request, restaurant_id):
             except Restaurant.DoesNotExist:
                 return HttpResponse(status=404)
             response_list = [
-<<<<<<< HEAD
                     {'id':review.id, 'content': review.content, 'rating': review.rating, 'date': review.date.strftime('%Y/%m/%d, %H:%M:%S')
                         } for review in Review.objects.all() if request.user.email == review.author.user.email]
             return HttpResponse(response_list, status=200) 
-=======
-                {
-                    'id': review.id,
-                    'content': review.content,
-                    'rating': review.rating,
-                    'date': review.date.strftime('%Y/%m/%d, %H:%M:%S')
-                }
-                for review in Review.objects.all()
-                if request.user.email == review.author.user.email]
-            return HttpResponse(response_list, status=200)
-
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
         elif request.method == 'POST':
             try:
                 req_data = json.loads(request.body.decode())
@@ -77,23 +63,9 @@ def get_post_my_review(request, restaurant_id):
                 restaurant = Restaurant.objects.get(id=restaurant_id)
             except Restaurant.DoesNotExist:
                 return HttpResponse(status=404)
-<<<<<<< HEAD
             date = datetime.now()
             author = Profile.objects.get(user=request.user)
             new_review = Review(restaurant=restaurant, author=author, content=content, rating=rating, date=date, site='atm') 
-=======
-
-            date = datetime.now()
-            author = Profile.objects.get(user=request.user)
-
-            new_review = Review(
-                restaurant=restaurant,
-                author=author,
-                content=content,
-                rating=rating,
-                date=date,
-                site='atm')
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
             new_review.save()
             response_dict = {
                 'id': new_review.id,
@@ -118,10 +90,6 @@ def edit_my_review(request, review_id):
                 target_review = Review.objects.get(id=review_id)
             except Review.DoesNotExist:
                 return HttpResponseNotFound('<h1>Page not found</h1>')
-<<<<<<< HEAD
-=======
-
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
             if request.user.email != target_review.author.user.email:
                 return HttpResponseForbidden()
             try:
@@ -134,11 +102,6 @@ def edit_my_review(request, review_id):
             target_review.rating = rating
             target_review.date = datetime.now()
             target_review.save()
-<<<<<<< HEAD
-            response_dict = {'id': target_review.id, 'content': target_review.content, 'rating': target_review.rating, 'date': target_review.date.strftime('%Y/%m/%d, %H:%M:%S')}
-            return HttpResponse(content=json.dumps(response_dict), status=200)  ## default status is 200 
-=======
-
             response_dict = {
                 'id': target_review.id,
                 'content': target_review.content,
@@ -147,29 +110,19 @@ def edit_my_review(request, review_id):
             return HttpResponse(
                 content=json.dumps(response_dict),
                 status=200)  # default status is 200
-
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
         elif request.method == 'DELETE':
             try:
                 target_review = Review.objects.get(id=review_id)
             except Review.DoesNotExist:
                 return HttpResponseNotFound('<h1>Page not Found</h1>')
-<<<<<<< HEAD
-=======
-
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
             if request.user.email != target_review.author.user.email:
                 return HttpResponseForbidden()
             response_dict = {'id': target_review.id}
             target_review.delete()
-<<<<<<< HEAD
-            return HttpResponse(content=json.dumps(response_dict), status=200)  ## default status is 200
-=======
 
             return HttpResponse(
                 content=json.dumps(response_dict),
                 status=200)  # default status is 200
->>>>>>> 56f09c3845e936ed1fce05b66fca712223b82f65
         else:
             return HttpResponseNotAllowed(['PUT', 'DELETE'])
     else:
