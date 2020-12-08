1569817cabc4dc27fcaa5c7c2a760608f3a69515
jtlho <jtlho@jtlhoui-MacBookPro.local>
2020-12-08 21:34:28 +0900

    fixed

--- frontend/src/store/actions/userActions/userActions.js
+++ frontend/src/store/actions/userActions/userActions.js
@@ -39,14 +39,11 @@ export const getUser = () => (dispatch) => axios
   .then((res) => dispatch(getUser_(res.data)))
   .catch((err) => alert('You need to login first!'));
 
-const postSignIn_ = () => ({
-  type: actionTypes.POST_SIGN_IN,
-});
 
 export const postSignIn = (userInfo) => (dispatch) => axios
   .post('/atm/sign-in/', userInfo)
   .then((res) => {
-    dispatch(postSignIn_());
+    dispatch({type: actionTypes.POST_SIGN_IN});
   })
   .catch((err) => alert('Login failed'));
 
@@ -62,15 +59,12 @@ export const postSignUp = (userInfo) => (dispatch) => axios
       }
     });
 
-const getSignOut_ = () => ({
-  type: actionTypes.GET_SIGN_OUT,
-})
 
 export const getSignOut = () => (dispatch) => axios
   .get('atm/sign-out/')
   .then((res) => {
     dispatch(push('/'));
-    dispatch(getSignOut_())
+    dispatch({type: GET_SIGN_OUT})
   })
   .catch((err) => {
     alert('sign-out failed!');
