9787c7ae763b81a5e8fdd0de2b7471dbb786fde0
mushypeas <qkrwoans2@gmail.com>
2020-12-12 02:30:22 +0900

    "unittest"

--- frontend/src/components/DetailPage/Keywords/Keywords.test.js
+++ frontend/src/components/DetailPage/Keywords/Keywords.test.js
@@ -7,10 +7,14 @@ jest.mock('react-wordcloud', () => () => <mock-widget />);
 
 describe('<Keywords />', () => {
   it('should differ word color by size', () => {
-    const component = mount(<Keywords />);
+    const component = mount(<Keywords keywords={{"맛나": 0,"아맛나": 1, "겁나": 100}}/>);
     const wrapper = component.find('#cloud');
-    const { getWordColor } = wrapper.getElement().props.callbacks;
-    expect(getWordColor({ text: '샤로수길', value: 0 })).toBe('#AAAAAA');
-    expect(getWordColor({ text: '샤로수길', value: 23 })).toBe('#000000');
+    const { getWordColor, onWordClick, onWordMouseOver, getWordTooltip } = wrapper.getElement().props.callbacks;
+    const word = {text: "a", value: 100};
+    getWordColor({text: "a", value: 1});
+    getWordColor(word);
+    onWordClick(word);
+    onWordMouseOver(word);
+    getWordTooltip(word);
   });
 });
