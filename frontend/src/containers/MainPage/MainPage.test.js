5548742d07342e984f7f7e4a8cae0fcae45b2955
mushypeas <qkrwoans2@gmail.com>
2020-12-12 14:44:38 +0900

    what the..

--- frontend/src/containers/MainPage/MainPage.test.js
+++ frontend/src/containers/MainPage/MainPage.test.js
@@ -51,7 +51,7 @@ const stubInitialState = {
       {
         id: 1,
         title: '안녕베트남',
-        rate: 4.78,https://img1.daumcdn.net/thumb/R1920x0.q100/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flocal%2Freview%2F2ce1e5c563f8149350b8e65fe1acab0da2ed287c7f7cca248b17784268585dd0
+        rate: 4.78,
         img_url_list: ['https://img1.daumcdn.net/thumb/R1920x0.q100/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flocal%2Freview%2F2ce1e5c563f8149350b8e65fe1acab0da2ed287c7f7cca248b17784268585dd0'],
         menu: ['짜장면 6000원', '탕수육 2000원'],
         time: '9;00-12:00',
