0c4b9084c723e062760beb83dff97db117852d93
sugsugsug <thddnrud2010@gmail.com>
2020-12-18 16:46:31 +0900

    :art: sonar

--- backend/AllTastesMatter/settings.py
+++ backend/AllTastesMatter/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'dar#yilu+rznpm2ez$)m(^jssl=^*-jv8z)@@653!nclu5bauc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
